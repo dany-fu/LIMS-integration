d03e560eaa2b42fbff41fc2acebc60e9f43621d5
Dany Fu <danyfu@bu.edu>
2020-06-20 19:11:34 -0400

    cleanup and get all sampleTypes

--- server.js
+++ server.js
@@ -1,93 +1,39 @@
-const http = require('follow-redirects').http;
+const axios = require("axios").default;
 
-module.exports.login = function login(username, password){
-  username = 'danyfu@bu.edu';
-  password = 'foobar';
-  //let auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
-  const data = JSON.stringify({
-    'username': username,
-    'password': password
-  });
+async function login(username, password) {
+  username = "danyfu@bu.edu";
+  password = "foobar";
 
-  let options = {
-    host: 'elabinventory.com',
-    path: '/api/v1/auth/user',
-    method: 'POST',
-    // Authorization: auth
-    // auth: `${username}:${password}`
-    headers: {'Content-Type': 'application/json',
-              'Accept': 'application/json',
-              // 'Content-Length': data.length
-              // 'Authorization': {
-              //   'username': username,
-              //   'password': password
-              //   }
-              }
-  };
-
-  let callback = function(response) {
-    let str = '';
-    response.on('data', function (chunk) {
-      str += chunk;
-    });
-
-    response.on('end', function () {
-      console.log('STATUS: ' + response.statusCode);
-      console.log(response);
-      console.log(str);
-    });
-  };
-
-  let req = http.request(options, callback);
-  req.write(data);
-  // console.log(req);
-  req.end();
-};
-
-
-
-
-
-// Using the request library which is deprecated
-const request = require('request');
-
-module.exports.login2 = function login2(username, password){
-  username = 'danyfu@bu.edu';
-  password = 'wrong';
-
-  request.post('https://www.elabinventory.com/api/v1/auth/user', {
-    json: {
-      'username': username,
-      'password': password
-    }
-  }, (error, res, body) => {
-    if (error) {
-      console.error(error);
-      return
-    }
-    console.log(`statusCode: ${res.statusCode}`);
-    console.log(body)
-  });
-
-};
-
-
-
-const axios = require('axios').default;
-
-module.exports.login3 = function login3(username, password){
-  username = 'danyfu@bu.edu';
-  password = 'wrong';
-
-  axios.post('https://www.elabinventory.com/api/v1/auth/user', {
-    'username': username,
-    'password': password
-  })
+  return axios
+    .post("https://us.elabjournal.com/api/v1/auth/user", {
+      username: username,
+      password: password,
+    })
     .then((res) => {
-      console.log(`statusCode: ${res.statusCode}`);
+      console.log(`statusCode: ${res.status}`);
+      console.log(res.data);
+      axios.defaults.headers.common['Authorization'] = res.data.token;
     })
     .catch((error) => {
-      console.error(error)
+      console.error(error);
     });
+}
 
-};
+module.exports.sample = async function getSampleTypes(){
+  await login();
+
+  axios.get("https://us.elabjournal.com/api/v1/sampleTypes")
+    .then(function (res) {
+      // handle success
+      console.log(res.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+
+
