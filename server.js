e13990244e38fc17ce56c47d7373c955d3f34dac
Dany Fu <danyfu@bu.edu>
2020-06-22 12:49:23 -0400

    get all patient samples

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const axios = require("axios").default;
+const ENDPOINTS = {
+  LOGIN: "https://us.elabjournal.com/api/v1/auth/user",
+  GET_SAMPLE_TYPES: "https://us.elabjournal.com/api/v1/sampleTypes",
+  CREATE_SAMPLE: "https://us.elabjournal.com/api/v1/samples?autoCreateMetaDefaults=true",
+  GET_ALL_PATIENT_SAMPLES: "https://us.elabjournal.com/api/v1/samples?sampleTypeID=33369"
+};
 
 async function login(username, password) {
   username = "danyfu@bu.edu";
   password = "foobar";
 
   return axios
-    .post("https://us.elabjournal.com/api/v1/auth/user", {
+    .post(ENDPOINTS.LOGIN, {
       username: username,
       password: password,
     })
@@ -22,7 +28,7 @@ async function login(username, password) {
 async function getSampleTypes(){
   await login();
 
-  axios.get("https://us.elabjournal.com/api/v1/sampleTypes")
+  axios.get(ENDPOINTS.GET_SAMPLE_TYPES)
     .then(function (res) {
       // handle success
       console.log(res.data);
@@ -36,24 +42,39 @@ async function getSampleTypes(){
     });
 }
 
-module.exports.sample = async function makeSample(){
+async function makeSample(){
   await login();
 
   axios
-  .post("https://us.elabjournal.com/api/v1/samples?autoCreateMetaDefaults=true", {
+    .post(ENDPOINTS.CREATE_SAMPLE, {
       sampleTypeID: 33369,
       name: 'dany-test-api-sample'
     })
     .then((res) => {
       console.log(`statusCode: ${res.status}`);
       console.log(res.data);
-      axios.defaults.headers.common['Authorization'] = res.data.token;
     })
     .catch((error) => {
       console.error(error);
     });
 
-};
+}
+
+module.exports.sample = async function getAllPatientSamples(){
+  await login();
+
+  axios.get(ENDPOINTS.GET_ALL_PATIENT_SAMPLES)
+    .then((res) => {
+      console.log(`statusCode: ${res.status}`);
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+}
+
+
 
 
 
